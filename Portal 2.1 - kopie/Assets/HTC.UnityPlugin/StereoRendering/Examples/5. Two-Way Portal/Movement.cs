b115fa9e0d49525369a3d17afffdc7caf1946b31
SaccoVaes <sacco.vaes@student.aventus.nl>
2019-12-02 16:32:53 +0100

    Werkende Portals

---
+++ Portal 2.1 - kopie/Assets/HTC.UnityPlugin/StereoRendering/Examples/5. Two-Way Portal/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public Transform eye;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            this.transform.position += eye.forward * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Portal 2.1 - kopie/Assets/MoveDestination.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDestination : MonoBehaviour
+{
+    public Transform PortalCaster;
+    public Transform PortalReceiver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = PortalReceiver.position - new Vector3(0, 0, 0.4f);
+        //Get the rotation from the caster.
+        Quaternion receiverrotation = PortalCaster.rotation;
+        Vector3 rotationVector = new Vector3(receiverrotation.x, (360.0f - receiverrotation.y),receiverrotation.z);
+
+        transform.rotation = Quaternion.Euler(rotationVector);
+    }
+
+}
